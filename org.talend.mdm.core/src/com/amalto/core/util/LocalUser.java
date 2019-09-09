457481be67aad57ae60f6ba68f48871d9a0d2e81
huawei.zhu <41101497+hw-zhu@users.noreply.github.com>
2019-09-09 10:16:00 +0800

    TMDM-13941 User with role System_interactive can access Data Model info via REST API even if no right via custom roles (#871)

--- org.talend.mdm.core/src/com/amalto/core/util/LocalUser.java
+++ org.talend.mdm.core/src/com/amalto/core/util/LocalUser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+ * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
  * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -43,6 +43,16 @@ public class LocalUser {
      */
     public static ILocalUser getLocalUser() throws XtentisException {
         return findLocalUser().getILocalUser();
+    }
+
+    /**
+     * Called by the Web Service, like DataService.
+     * @param isWebServiceAccess
+     * @return The Local User
+     * @throws XtentisException
+     */
+    public static ILocalUser getLocalUser(boolean isWebServiceAccess) throws XtentisException {
+        return findLocalUser().getILocalUser(isWebServiceAccess);
     }
 
     public static void resetLocalUsers() throws XtentisException {
